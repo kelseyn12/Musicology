31131ce6f895cc7eb524c548afd2422945e7cb6e
kelsey nocek <kelseyn12@hotmail.com>
2022-02-20 18:48:23 -0600

    added authorization

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
@@ -7,6 +8,7 @@ class Post(models.Model):
     intro = models.TextField()
     body = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['-date_added']
