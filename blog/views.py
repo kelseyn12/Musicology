11e4c2602b81d3e98449b891419efbfdb1897123
kelsey nocek <kelseyn12@hotmail.com>
2022-02-20 20:01:51 -0600

    added authorization

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def frontpage(request):
-    return render(request, 'frontpage.html')
+    posts = Post.objects.all()
+    return render(request, 'frontpage.html', {'posts': posts})
+
+def about(request):
+    return render(request, 'about.html')
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug=slug)
+
+    return render(request, 'blog/post_detail.html', {'post': post})
--- musicology/urls.py
+++ musicology/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import frontpage
+from blog.views import frontpage, post_detail, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', frontpage, name='frontpage'),
-
+    path('about/', about, name='about'),
+    path('<slug:slug>/', post_detail, name='post_detail')
+    
 ]
